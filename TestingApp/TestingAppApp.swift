0d352f4fa2061a5df4e2cff0810bb0f3d8110efa
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-10 14:38:06 +0300

    Initial Commit

---
+++ TestingApp/TestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAppApp.swift
+//  TestingApp
+//
+//  Created by Yasin Şükrü Tan on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
